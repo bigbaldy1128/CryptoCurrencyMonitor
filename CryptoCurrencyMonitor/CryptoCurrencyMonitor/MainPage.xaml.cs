3270512c5ad3687d51391264184a57cf18874d53
bigbaldy <bigbaldy@163.com>
2021-06-28 15:01:25 +0800

    update business frame

--- CryptoCurrencyMonitor/CryptoCurrencyMonitor/MainPage.xaml.cs
+++ CryptoCurrencyMonitor/CryptoCurrencyMonitor/MainPage.xaml.cs
@@ -1,29 +1,23 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Xamarin.Forms;
-using Flurl.Http;
-
-using Newtonsoft.Json.Linq;
-using System.IO;
-using Newtonsoft.Json;
+using CryptoCurrencyMonitor.Business;
 
 namespace CryptoCurrencyMonitor
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, IMainPage
     {
+        DataHelper dataHelper;
         public MainPage()
         {
             InitializeComponent();
+            dataHelper = new DataHelper(this, Business.Type.OperatingSystem.ANDROID);
             Handle_Clicked_Get_Data(null, null);
         }
-
-        private LocalData localData;
-        private string localFilePath;
 
         async void Handle_Clicked_Get_Data(object sender, System.EventArgs e)
         {
             button_get_data.IsEnabled = false;
-            var data = await GetResult();
+            var data = await dataHelper.GetResult();
             editor_address.Text = data.address;
             label_quantity.Text = data.quantity.ToString("N0");
             label_delta_quantity.Text = "";
@@ -53,101 +47,17 @@ namespace CryptoCurrencyMonitor
             label_quantity.Text = null;
             label_delta_quantity.Text = null;
             label_timestamp.Text = null;
-            ClearLocalData();
+            dataHelper.ClearLocalData();
         }
 
-        private class RemoteData
+        public void SetLableTimestamp(string str)
         {
-            public string address;
-            public long quantity;
-            public long deltaQuantity;
+            label_timestamp.Text = str;
         }
 
-        private class LocalData
+        public new Task DisplayAlert(string title, string message, string cancel)
         {
-            public string address;
-            public long firstQuantity;
-            public DateTime timestamp;
+            return base.DisplayAlert("Alert", "Address has been changed", "OK");
         }
-
-        private async Task<RemoteData> GetResult()
-        {
-            RemoteData ret = new RemoteData();
-            string json = await "https://dncapi.bqrank.net/api/v3/coin/holders?code=dogecoin&webp=1".GetStringAsync();
-            JObject jObject = JObject.Parse(json);
-            string address = (string)jObject["data"]["flows"][0]["address"];
-
-            if (localData != null && localData.address != null && localData.address != address)
-            {
-                await DisplayAlert("Alert", "Address has been changed", "OK");
-                localData = null;
-            }
-
-            ret.address = address;
-
-            long quantity = (long)double.Parse(await ("https://dogechain.info/chain/Dogecoin/q/addressbalance/" + address).GetStringAsync());
-            ret.quantity = quantity;
-
-            LoadLocalData();
-            if (localData.firstQuantity != 0)
-            {
-                ret.deltaQuantity = quantity - localData.firstQuantity;
-            }
-            else
-            {
-                localData.firstQuantity = quantity;
-                localData.address = address;
-                SaveLocalData();
-            }
-            return ret;
-        }
-
-        private string GetLocalFilePath()
-        {
-            if (localFilePath != null)
-            {
-                return localFilePath;
-            }
-            var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            localFilePath = Path.Combine(basePath, "db.json");
-            return localFilePath;
-        }
-
-        private void LoadLocalData()
-        {
-            if (localData != null)
-            {
-                return;
-            }
-            if (!File.Exists(GetLocalFilePath()))
-            {
-                localData = new LocalData();
-                return;
-            }
-            string json = File.ReadAllText(GetLocalFilePath());
-            localData = JsonConvert.DeserializeObject<LocalData>(json);
-            if (localData.timestamp != null)
-            {
-                label_timestamp.Text = localData.timestamp.ToString("yyyy-MM-dd HH:mm");
-            }
-        }
-
-        private void SaveLocalData()
-        {
-            localData.timestamp = DateTime.Now;
-            label_timestamp.Text = localData.timestamp.ToString("yyyy-MM-dd HH:mm");
-            string json = JsonConvert.SerializeObject(localData);
-            File.WriteAllText(GetLocalFilePath(), json);
-        }
-
-        private void ClearLocalData()
-        {
-            localData = null;
-            if (File.Exists(GetLocalFilePath()))
-            {
-                File.Delete(GetLocalFilePath());
-            }
-        }
-
     }
 }
